e03fb5491540a0009141f2078838f04905a5b71a
sergei <s.vds.work@yandex.ru>
2023-06-15 19:35:20 +0300

    start

---
+++ docker-compose-deploy.sh
@@ -0,0 +1,7 @@
+cp .env.example .env
+chmod 766 .env
+docker-compose build app
+docker-compose up -d
+docker-compose exec app php artisan key:generate
+docker-compose exec app composer install
+
---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    hey
+</template>
+
+<script>
+export default {
+    name: "App.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
