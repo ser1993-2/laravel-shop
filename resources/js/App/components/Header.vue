04055cb5190ede5485ab0925745c3f74117ed928
Sergey Ageev <=>
2023-06-29 00:21:22 +0300

    space

--- resources/js/App/components/Header.vue
+++ resources/js/App/components/Header.vue
@@ -2,6 +2,7 @@
     <header class="p-3 mb-3 border-bottom">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
                 <Logo/>
 
                 <Menu/>
